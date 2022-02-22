a5131cc520bc45cb4dc86a9ae3a6a8594acfb2d9
Naufal Dzakwan <n.dzakwan9@gmail.com>
2022-02-22 15:14:16 +0700

    PAE

---
+++ src/test/java/com/Refreshing/Refreshing/RefreshingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Refreshing.Refreshing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
